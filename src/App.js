004b28a80e709f6a7497ee4b8bedf17778e13238
Rudolf Benadie <rudolf.benadie@hotmail.com>
2020-04-29 07:06:22 +0200

    Restart styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/scss/main.scss?v=1.1.0";
