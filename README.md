1d4a651b401d9c153fb14f68ffd72d61943cd3f6
Rudolf Benadie <rudolf.benadie@hotmail.com>
2020-05-03 13:16:52 +0200

    Refactor: Fontawesome

--- README.md
+++ README.md
@@ -11,3 +11,4 @@
   1. npm install react-redux
   1. npm install redux-persist
   1. npm install redux-thunk
+  1. npm i --save @fortawesome/fontawesome-svg-core  @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome @fortawesome/free-brands-svg-icons
--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import { connect } from 'react-redux';
-
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/scss/lunularia.scss?v=1.1.0";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
 
 import BaseLayout from "./layouts/baseLayout";
 import Authenticate from "./layouts/authenticate";
+
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { connect } from 'react-redux';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faTachometerAlt, faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+library.add(fab, faTachometerAlt, faCheckSquare, faCoffee);
 
 class App extends Component {
 
